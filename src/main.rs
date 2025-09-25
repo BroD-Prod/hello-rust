7c02670d55e1c81eed8c752b540d287293316e09
Brody Roth <brodyroth23@gmail.com>
2025-09-25 19:37:17 -0400

    Adding ability to pass arguments and not exit after singular command with loop. Helps to understand mutable vs non-mutable variable type.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::{io::stdin, process::Command};
+use std::{io::{stdin, stdout, Write}, process::Command};
 
 fn main(){
+    loop{
+    print!(">");
+    let _ = stdout().flush();
+
     let mut s = String::new();
     stdin().read_line(&mut s).unwrap();
 
-    let command = s.trim();
+    let mut command_parts = s.trim().split_whitespace();
+    let command = command_parts.next().unwrap();
+    let args = command_parts;
 
-    Command::new(command).spawn().unwrap();
+    let mut child = Command::new(command).args(args).spawn().unwrap();
+    
+    let _ = child.wait();
+    }
 }
