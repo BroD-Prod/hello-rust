37aeeb8761d12614a9b328d8f586662d0f12df77
Brody Roth <brodyroth23@gmail.com>
2025-09-30 11:36:39 -0400

    Adding match case to allow for 'cd' implentation. Adding exit to match case to allow for graceful exit as well.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,22 @@ use std::{io::{stdin, stdout, Write}, process::Command};
 
 fn main(){
     loop{
-    print!(">");
-    let _ = stdout().flush();
+        print!(">");
+        let _ = stdout().flush();
 
-    let mut s = String::new();
-    stdin().read_line(&mut s).unwrap();
+        let mut s = String::new();
+        stdin().read_line(&mut s).unwrap();
 
-    let mut command_parts = s.trim().split_whitespace();
-    let command = command_parts.next().unwrap();
-    let args = command_parts;
+        let mut command_parts = s.trim().split_whitespace();
+        let command = command_parts.next().unwrap();
+        let args = command_parts;
 
-    let mut child = Command::new(command).args(args).spawn().unwrap();
-    
-    let _ = child.wait();
+        match command {
+                "exit" => break,
+                command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+                let _ = child.wait();
+            }
+        }
     }
 }
